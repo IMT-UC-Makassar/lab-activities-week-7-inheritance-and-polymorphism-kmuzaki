466804938d701d285443c66ce65ec65afca1e363
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-04-29 15:14:34 +0800

    moveFilesAndInitSavingsAccount

--- app/src/main/java/oop/lab/BankAccount.java
+++ app/src/main/java/oop/lab/BankAccount.java
@@ -1,11 +1,14 @@
+package oop.lab;
+
 public abstract class BankAccount {
     private String accountNumber;
     private String accountHolder;
     private Double balance;
 
-    public BankAccount (String number, String name){
+    public BankAccount (String number, String name, double moneh){
         this.accountNumber = number;
         this.accountHolder = name;
+        this.balance = moneh;
     }
 
     public void deposit(double amount){
