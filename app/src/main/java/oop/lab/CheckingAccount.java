73df1d61ab57b1436684dc2feb76d5b19c52e554
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-04-29 15:16:35 +0800

    initCheckingAcc

---
+++ app/src/main/java/oop/lab/CheckingAccount.java
@@ -0,0 +1,16 @@
+package oop.lab;
+
+public class CheckingAccount extends BankAccount {
+
+    public CheckingAccount(String number, String name, double moneh) {
+        super(number, name, moneh);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void calculateInterest() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateInterest'");
+    }
+    
+}
