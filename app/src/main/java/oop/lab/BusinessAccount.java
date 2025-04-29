5a52242d6eb556a99024c93e899d95da143c8389
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-04-29 15:25:34 +0800

    initBusinessAccount

---
+++ app/src/main/java/oop/lab/BusinessAccount.java
@@ -0,0 +1,31 @@
+package oop.lab;
+
+public class BusinessAccount extends BankAccount{
+    private String loanStatus;
+
+    // public BusinessAccount(String number, String name, double moneh) {
+    //     super(number, name, moneh);
+    //     //TODO Auto-generated constructor stub
+    // }
+
+    public BusinessAccount(String number, String name, double moneh, String loan) {
+        super(number, name, moneh);
+        //TODO Auto-generated constructor stub
+    }
+
+
+    @Override
+    public void calculateInterest() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculateInterest'");
+    }
+
+    public void applyForLoan(double amount){
+
+    }
+
+    public String checkLoanStatus(){
+        return "You have " + loanStatus + " for loans";
+    }
+    
+}
